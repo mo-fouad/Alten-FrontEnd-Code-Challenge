45d968ac8510c4dc8e8ced38b625428b3d33c4aa
MoFouad <mo.fouad.saga@gmail.com>
2019-09-07 20:41:54 +0200

    adding Fix to Server

--- server/server.js
+++ server/server.js
@@ -1,30 +1,28 @@
-const app = require('express')();
-const server = require('http').Server(app);
-const io = require('socket.io')(server);
-const port = process.env.PORT || 4002;
+const app = require("express")();
+const server = require("http").Server(app);
+const io = require("socket.io")(server);
+const port = process.env.PORT || 4001;
 const db = require("./database/db");
 
-app.get('/', function (req, res) {
-    res.sendFile(__dirname + '/index.html');
+app.get("/", function(req, res) {
+  res.sendFile(__dirname + "/index.html");
 });
 
-
 const getApiAndEmit = async socket => {
-    socket.emit("FromAPI", {Data: "data"})
+  socket.emit("FromAPI", { Data: "data" });
 };
-
 
 /**
  * Sending Updated Data every 1 sec .. just to fake the Real-Life Data
  */
-io.on('connection', (socket) => {
-    console.log("New client connected");
-    setInterval(() => {
-        socket.emit('dataUpdated', updateDBVehicles(db))
-    }, 1000);
-    socket.on("disconnect", () => {
-        console.log("Client disconnected");
-    });
+io.on("connection", socket => {
+  console.log("New client connected");
+  setInterval(() => {
+    socket.emit("dataUpdated", updateDBVehicles(db));
+  }, 1000);
+  socket.on("disconnect", () => {
+    console.log("Client disconnected");
+  });
 });
 
 // Manipulating Data to send Fake Status
@@ -35,15 +33,15 @@ io.on('connection', (socket) => {
  * @returns db // the updaded Data base with random Status
  *
  */
-let updateDBVehicles = (db) => {
-    let vehiclesJson = [];
-    if (db.vehicles) {
-        db.vehicles.forEach((ele) => {
-            ele.status = getRandomStatus();
-            vehiclesJson.push(ele);
-        });
-    }
-    return db
+let updateDBVehicles = db => {
+  let vehiclesJson = [];
+  if (db.vehicles) {
+    db.vehicles.forEach(ele => {
+      ele.status = getRandomStatus();
+      vehiclesJson.push(ele);
+    });
+  }
+  return db;
 };
 
 /**
@@ -51,11 +49,9 @@ let updateDBVehicles = (db) => {
  * @returns {string}
  */
 let getRandomStatus = () => {
-    let randomNumber = Math.random();
-    if (randomNumber >= 0.5)
-        return "is-active";
-    else
-        return "not-active";
+  let randomNumber = Math.random();
+  if (randomNumber >= 0.5) return "is-active";
+  else return "not-active";
 };
 
 server.listen(port, () => console.log(`Listening on port ${port}`));
